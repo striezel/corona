d6a3bb898bf1ad09fd9170d0caafdc389560fcf2
Dirk Stolle <striezel-dev@web.de>
2022-08-14 18:49:17 +0200

    fix new clippy warning for derived PartialEq without Eq

--- src/collect/api/mod.rs
+++ src/collect/api/mod.rs
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of the Corona numbers website generator.
-    Copyright (C) 2021  Dirk Stolle
+    Copyright (C) 2021, 2022  Dirk Stolle
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -18,7 +18,7 @@
 pub mod disease_sh;
 
 /// Determines the amount of data that shall be collected.
-#[derive(PartialEq)]
+#[derive(PartialEq, Eq)]
 pub enum Range
 {
   Recent, // only collect recent data
